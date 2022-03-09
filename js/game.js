1173c0f8bbbead7b1eaf98190c537b7c02996f6d
Olimpia Spyra <olimpia.spyra@gmail.com>
2022-03-09 22:25:23 +0100

    Add reveal field and flags

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
-import { Cell } from "./Cell";
+import { Cell } from "./Cell.js";
+import { UI } from "./UI.js";
 
-class Game {
+class Game extends UI {
   #config = {
     easy: {
       rows: 8,
@@ -25,7 +26,12 @@ class Game {
 
   #cells = [];
 
+  #cellsElements = null;
+
+  #board = null;
+
   initializeGame() {
+    this.#handleElements();
     this.#newGame();
   }
 
@@ -38,7 +44,25 @@ class Game {
     this.#numberOfCols = cols;
     this.#numberOfMines = mines;
 
+    this.#setStyles();
+
     this.#generateCells();
+    this.#renderBoard();
+
+    this.#cellsElements = this.getElements(this.UiSelectors.cell);
+
+    this.#addCellsEventListeners();
+  }
+
+  #handleElements() {
+    this.#board = this.getElement(this.UiSelectors.board);
+  }
+
+  #addCellsEventListeners() {
+    this.#cellsElements.forEach((element) => {
+      element.addEventListener("click", this.#handleCellClick);
+      element.addEventListener("contextmenu", this.#handleCellContextMenu);
+    });
   }
 
   #generateCells() {
@@ -48,6 +72,43 @@ class Game {
         this.#cells[row].push(new Cell(col, row));
       }
     }
+  }
+
+  #renderBoard() {
+    this.#cells.flat().forEach((cell) => {
+      this.#board.insertAdjacentHTML("beforeend", cell.createElement());
+      cell.element = cell.getElement(cell.selector);
+    });
+  }
+
+  #handleCellClick = (e) => {
+    const target = e.target;
+    const rowIndex = parseInt(target.getAttribute("data-y"), 10);
+    const colIndex = parseInt(target.getAttribute("data-x"), 10);
+
+    const cell = this.#cells[rowIndex][colIndex];
+
+    this.#cells[rowIndex][colIndex].revealCell();
+  };
+
+  #handleCellContextMenu = (e) => {
+    e.preventDefault();
+    const target = e.target;
+    const rowIndex = parseInt(target.getAttribute("data-y"), 10);
+    const colIndex = parseInt(target.getAttribute("data-x"), 10);
+
+    const cell = this.#cells[rowIndex][colIndex];
+
+    if (cell.isReveal) return;
+
+    cell.toggleFlag();
+  };
+
+  #setStyles() {
+    document.documentElement.style.setProperty(
+      "--cells-in-row",
+      this.#numberOfCols
+    );
   }
 }
 
